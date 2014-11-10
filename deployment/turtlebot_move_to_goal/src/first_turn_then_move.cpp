1c2a53399a4c9fc56b840148e5e22fec1b27ad65
Katherine <turtlebot@boticellibot.(none)>
2014-11-10 13:36:17 -0800

    Get rid of Kalman

--- deployment/turtlebot_move_to_goal/src/first_turn_then_move.cpp
+++ deployment/turtlebot_move_to_goal/src/first_turn_then_move.cpp
@@ -6,13 +6,10 @@
 #include <geometry_msgs/Twist.h>
 #include <nav_msgs/Odometry.h>
 #include "geometry_msgs/PoseWithCovarianceStamped.h"
-#include <geometry_msgs/PoseStamped.h>
 #include <tf/tf.h>
-#include <turtlebot_deployment/PoseWithName.h>
 
 const double PI = 3.14159265;
 
-bool flag1=false;
 class FirstTurnThenMove
 {
 public:
@@ -20,7 +17,7 @@ public:
 private:
 
     // Methods
-    void poseCallback(const turtlebot_deployment::PoseWithName::ConstPtr& pose);
+    void poseCallback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr&);
     void goalCallback(const geometry_msgs::PoseStampedConstPtr&);
     void odomCallback(const nav_msgs::OdometryConstPtr&);
 
@@ -37,8 +34,6 @@ private:
     nav_msgs::Odometry odom_;
     geometry_msgs::PoseStamped goalPose_;
     geometry_msgs::TwistPtr cmd_vel_;
-  //  geometry_msgs::PoseStamped Kalman_pose_;
-    geometry_msgs::Pose samePose;
 
     double max_drive_speed_,
     max_turn_speed_,
@@ -61,17 +56,13 @@ FirstTurnThenMove::FirstTurnThenMove():
     ph_.param("angle_tolerance", angle_tolerance_, angle_tolerance_);
     ph_.param("distance_tolerance", distance_tolerance_, distance_tolerance_);
 
-    pos_sub_ = nh_.subscribe<turtlebot_deployment::PoseWithName>("afterKalman", 1, &FirstTurnThenMove::poseCallback, this);
+    pos_sub_ = nh_.subscribe<geometry_msgs::PoseWithCovarianceStamped>("amcl_pose", 1, &FirstTurnThenMove::poseCallback, this);
     goal_sub_ = nh_.subscribe<geometry_msgs::PoseStamped>("move_base_simple/goal",1, &FirstTurnThenMove::goalCallback, this);
-    //odom_sub_ = nh_.subscribe<nav_msgs::Odometry>("odom",1, &FirstTurnThenMove::odomCallback, this);
-    vel_pub_ = nh_.advertise<geometry_msgs::Twist>("mobile_base/commands/velocity", 1, true);
-    //timer_ = nh_.createTimer(ros::Duration(0.05), boost::bind(&FirstTurnThenMove::update, this));
-    if(flag1==0){
-	cmd_vel_->angular.z=0;
-    	cmd_vel_->linear.x=0;
-    	vel_pub_.publish(cmd_vel_);
-    }
+    //    odom_sub_ = nh_.subscribe<nav_msgs::Odometry>("odom",1, &FirstTurnThenMove::odomCallback, this);
 
+    vel_pub_ = nh_.advertise<geometry_msgs::Twist>("mobile_base/commands/velocity", 1, true);
+
+    //    timer_ = nh_.createTimer(ros::Duration(0.05), boost::bind(&FirstTurnThenMove::update, this));
 }
 
 double FirstTurnThenMove::square(double x){
@@ -87,29 +78,29 @@ void FirstTurnThenMove::odomCallback(const nav_msgs::OdometryConstPtr & odom){
     odom_ = *odom;
 }
 
-void FirstTurnThenMove::poseCallback(const turtlebot_deployment::PoseWithName::ConstPtr& posePtr)
+void FirstTurnThenMove::poseCallback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& pose)
 {
-    flag1=true;
-    if ( got_goal_ )
-    {
-        samePose=posePtr->pose;
+    if ( got_goal_ ){
         // Calculate yaw angle
-        double orientation = tf::getYaw(samePose.orientation);
-        std::cout << "heading = " << orientation << "\n";
+        double orientation = tf::getYaw(pose->pose.pose.orientation);
+        //std::cout << "heading = " << orientation << "\n";
+	std::cout << "my_x = " << pose->pose.pose.position.x << " ;my_y = " << pose->pose.pose.position.y << "\n";
+	std::cout << "goal_x = " << goalPose_.pose.position.x << " ;goal_y = " << goalPose_.pose.position.y << "\n";
 
         // Calculate direction from location to goal position
-        double dy = goalPose_.pose.position.y - samePose.position.y;
-        double dx = goalPose_.pose.position.x - samePose.position.x;
+        double dy = goalPose_.pose.position.y - pose->pose.pose.position.y;
+        double dx = goalPose_.pose.position.x - pose->pose.pose.position.x;
         double goalDirection = atan2(dy,dx);
 
         double distance_to_goal = sqrt(square(dx)+square(dy));
-        std::cout << "distance to goal = " << distance_to_goal << "\n";
+        //std::cout << "distance to goal = " << distance_to_goal << "\n";
 
         // Calculate angle from orientation to goal
         double dth = goalDirection - orientation;
+	std::cout << "my_orientation = " << orientation << " ;goal_orientation= " << goalDirection << "\n";
 
         // Make sure that the robot always turns over the smallest angle
-  	if ( dth > PI ) {
+        if ( dth > PI ) {
             dth = dth - 2*PI;
         } else if ( dth < -PI ) {
             dth = dth + 2*PI;
@@ -118,22 +109,20 @@ void FirstTurnThenMove::poseCallback(const turtlebot_deployment::PoseWithName::C
         std::cout << "angle to goal = " << dth << "\n\n";
 
         if ( dth < - angle_tolerance_ ) {
-	  // cmd_vel_->angular.z = 0.0; 
-	  cmd_vel_->angular.z = -max_turn_speed_;
-          cmd_vel_->linear.x = 0.0;
+            cmd_vel_->angular.z = -max_turn_speed_;
+            cmd_vel_->linear.x = 0.0;
         }
         else if ( dth > angle_tolerance_ ) {
-	  //cmd_vel_->angular.z = 0.0;
- 	  cmd_vel_->angular.z = max_turn_speed_;
-          cmd_vel_->linear.x = 0.0;
+            cmd_vel_->angular.z = max_turn_speed_;
+            cmd_vel_->linear.x = 0.0;
         }
         else if ( distance_to_goal > distance_tolerance_ ){
             cmd_vel_->angular.z = 0.0;
             cmd_vel_->linear.x = -max_drive_speed_;
         }
         else {
-          cmd_vel_->angular.z = 0.0;
-          cmd_vel_->linear.x = 0.0;
+            cmd_vel_->angular.z = 0.0;
+            cmd_vel_->linear.x = 0.0;
         }
     }
     else {
@@ -145,7 +134,6 @@ void FirstTurnThenMove::poseCallback(const turtlebot_deployment::PoseWithName::C
     vel_pub_.publish(cmd_vel_);
 }
 
-
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "FirstTurnThenMove");
@@ -153,4 +141,5 @@ int main(int argc, char **argv)
 
     ros::spin();
 }
+
 
