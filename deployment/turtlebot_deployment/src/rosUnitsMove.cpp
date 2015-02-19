82cbddc0a065d030e3c0b47c9c4a5494525d10f7
aaronma37 <aam021@eng.ucsd.edu>
2015-02-19 10:14:41 -0800

    Update rosUnitsMove.cpp

--- deployment/turtlebot_deployment/src/rosUnitsMove.cpp
+++ deployment/turtlebot_deployment/src/rosUnitsMove.cpp
@@ -13,18 +13,18 @@ int main(int argc, char **argv)
   
 ros::init(argc, argv, "rosUnitsMove");
 std::cout<<"Checkpoint";
-geometry_msgs::TwistPtr cmd_vel_;
+geometry_msgs::Twist cmd_vel_;
   ros::NodeHandle ph_, nh_;
   ros::Publisher vel_pub_;
   vel_pub_ = nh_.advertise<geometry_msgs::Twist>("mobile_base/commands/velocity", 1, true);
-  cmd_vel_->angular.z = 0.0;
-  cmd_vel_->linear.x = 1;
+  cmd_vel_.angular.z = 0.0;
+  cmd_vel_.linear.x = 1;
 
 vel_pub_.publish(cmd_vel_);
-/*sleep(5000);
+sleep(5000);
 cmd_vel_->angular.z=0;
 cmd_vel_->linear.x=0;
 vel_pub_.publish(cmd_vel_);
-*/return 0;
+return 0;
 
 }
