5f7ff1d6e19d3eb82d6a2d8817fb2d1f0eda82f3
aaronma37 <aam021@eng.ucsd.edu>
2015-02-19 11:33:12 -0800

    Update rosUnitsMove.cpp

--- deployment/turtlebot_deployment/src/rosUnitsMove.cpp
+++ deployment/turtlebot_deployment/src/rosUnitsMove.cpp
@@ -19,9 +19,9 @@ geometry_msgs::Twist cmd_vel_;
   ros::Publisher vel_pub_;
   vel_pub_ = nh_.advertise<geometry_msgs::Twist>("mobile_base/commands/velocity", 1, true);
   cmd_vel_.angular.z = 0.0;
-  cmd_vel_.linear.x = 1;
+  cmd_vel_.linear.x = .1;
   int x=1;
-  while (x<25){
+  while (x<5){
 vel_pub_.publish(cmd_vel_);
 x=x+1;
 sleep(1);
