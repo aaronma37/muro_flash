28c8663479ad8bc8ad740de8c418c76a5be5c154
aaronma37 <aam021@eng.ucsd.edu>
2015-02-19 18:55:34 -0800

    Update cyclic_pursuit.cpp

--- deployment/turtlebot_deployment/src/cyclic_pursuit.cpp
+++ deployment/turtlebot_deployment/src/cyclic_pursuit.cpp
@@ -14,7 +14,7 @@ Cyclic Pursuit
 
 //Declare Variables
 
-
+/*
 // Construct Node Class
 class cyclicPursuit
 {
@@ -25,11 +25,11 @@ private:
 
 void poseCallback(const geometry_msgs::Pose::ConstPtr&);
 // ROS stuff
-ros::NodeHandle ph_, nh_;
+
 ros::Subscriber pos_sub_;
-ros::Publisher vel_pub_;
+
 // Other member variables
-geometry_msgs::Twist cmd_vel_;
+
 geometry_msgs::Twist robVel;
 geometry_msgs::Pose pose;
 double velocity;
@@ -38,19 +38,24 @@ double velocity;
 cyclicPursuit::cyclicPursuit()
 {
 pos_sub_ = nh_.subscribe<geometry_msgs::Pose>("/position", 1, &cyclicPursuit::poseCallback, this);
-vel_pub_ = nh_.advertise<geometry_msgs::Twist>("/velocity", 1, true);
 }
 
 
 void cyclicPursuit::poseCallback(const geometry_msgs::Pose::ConstPtr& pose)
 {
 //THIS IS WHERE CYCLIC PURSUIT ALGORITHM GOES
-cmd_vel_.linear.x=10;
-	vel_pub_.publish(cmd_vel_);
-}
+
+}*/
 int main(int argc, char **argv)
 {
 ros::init(argc, argv, "cyclic_pursuit");
-cyclicPursuit cp;
-ros::spin();
+ros::NodeHandle ph_, nh_;
+ros::Publisher vel_pub_;
+geometry_msgs::Twist cmd_vel_;
+vel_pub_ = nh_.advertise<geometry_msgs::Twist>("/velocity", 1, true);
+cmd_vel_.linear.x=10;
+while (1==1){
+vel_pub_.publish(cmd_vel_);
+//cyclicPursuit cp;
+}ros::spin();
 }
