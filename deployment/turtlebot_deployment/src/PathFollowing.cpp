5cf6d0c2ba80f131df61c0ebf99fa88ebbde702d
aaronma37 <aam021@eng.ucsd.edu>
2015-02-23 12:32:22 -0800

    Update PathFollowing.cpp

--- deployment/turtlebot_deployment/src/PathFollowing.cpp
+++ deployment/turtlebot_deployment/src/PathFollowing.cpp
@@ -99,7 +99,7 @@ while(1==1){
 			double u1=robVel_;
 			double u2=robVel_/r;
 			std::cout<<"initial angular velocity: \n"<<u2<<"\n\n";
-			//u2=u2-k*(r*x1*cos(orientation)+r*x2*sin(orientation))/167/167; //check orientation units
+			u2=u2-k*(r*x1*cos(orientation)+r*x2*sin(orientation))/167/167; //check orientation units
 			std::cout<<"final angular velocity: \n"<<u2<<"\n\n";
 			cmd_vel_.linear.x=(u1/167);
 			cmd_vel_.angular.z=(u2*1.45);
