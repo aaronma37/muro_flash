6be5cef9a51dcd50e926469f230b01ae9d6867d3
aaronma37 <aam021@eng.ucsd.edu>
2015-02-23 19:16:30 -0800

    Update rosUnitsMove.cpp

--- deployment/turtlebot_deployment/src/rosUnitsMove.cpp
+++ deployment/turtlebot_deployment/src/rosUnitsMove.cpp
@@ -40,7 +40,7 @@ geometry_msgs::Twist cmd_vel_;
         vel_pub_.publish(cmd_vel_);
         usleep(10000);
         if (x==100){flip=-1;}
-        if (x==0){flip=1};
+        if (x==0){flip=1;}
         x=x+flip;
           } 
     sleep(10);
