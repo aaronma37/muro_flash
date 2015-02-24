c52141da9ab80f883a28487a7ef2250c56891cb7
aaronma37 <aam021@eng.ucsd.edu>
2015-02-23 18:29:41 -0800

    Update rosUnitsMove.cpp

--- deployment/turtlebot_deployment/src/rosUnitsMove.cpp
+++ deployment/turtlebot_deployment/src/rosUnitsMove.cpp
@@ -31,7 +31,7 @@ geometry_msgs::Twist cmd_vel_;
   while(1==1){
     time(&begin);
           while (difftime(time(&timer),begin)<100){
-            z=x%2+1;
+            z=x%50+1;
           cmd_vel_.angular.z = speed/z;
         vel_pub_.publish(cmd_vel_);
         usleep(10);
