1c43198e553989c9dbe1886c840cbe915feb5acc
aaronma37 <aam021@eng.ucsd.edu>
2015-03-03 18:17:33 -0800

    Update cyclic_pursuit.cpp

--- deployment/turtlebot_deployment/src/cyclic_pursuit.cpp
+++ deployment/turtlebot_deployment/src/cyclic_pursuit.cpp
@@ -72,7 +72,7 @@ x0=0;
 x2=0;
 while (1==1){
     //ros::spinOnce();
-    std::cout<<"@@@@@radians:"<<radN;
+    std::cout<<"@@@@@radians:"<<radN<<;
     cmd_vel_.linear.x=1;//10*k*(radN);
     vel_pub_.publish(cmd_vel_);
     usleep(100000);
