cf17b104644f97a4e6f1d0b4dd4874cfb0a24295
aaronma37 <aam021@eng.ucsd.edu>
2015-03-05 13:24:04 -0800

    Update ekf.cpp

--- deployment/turtlebot_deployment/src/ekf.cpp
+++ deployment/turtlebot_deployment/src/ekf.cpp
@@ -259,7 +259,7 @@ Z << agentVector[iTemp].x,agentVector[iTemp].y,agentVector[iTemp].theta;
 
 //Calibration 
 if (counter11>10){
-XT << XT(0)+agentVector[iTemp].velo*167/T*cos(XT(2)),XT(1)+agentVector[iTemp].velo*167/T*sin(XT(2)),XT(2)+agentVector[iTemp].omega*57/52/T/OmegaC;
+XT << XT(0)+agentVector[iTemp].velo*167/T*cos(XT(2))/OmegaD,XT(1)+agentVector[iTemp].velo*167/T*sin(XT(2))/OmegaD,XT(2)+agentVector[iTemp].omega*57/52/T/OmegaC;
 }
 X << X(0)+agentVector[iTemp].velo*167/T*cos(X(2)),X(1)+agentVector[iTemp].velo*167/T*sin(X(2)),X(2)+agentVector[iTemp].omega*57/52/T/OmegaC;
 cout<<"Velocity: "<<agentVector[iTemp].velo*167/T<<"\n";
