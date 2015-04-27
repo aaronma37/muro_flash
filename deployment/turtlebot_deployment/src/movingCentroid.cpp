d3d56b7d6752fc351a9319b8924bb756fc2a5576
aaronma37 <aam021@eng.ucsd.edu>
2015-04-27 12:47:55 -0700

    Update movingCentroid.cpp

--- deployment/turtlebot_deployment/src/movingCentroid.cpp
+++ deployment/turtlebot_deployment/src/movingCentroid.cpp
@@ -1,6 +1,15 @@
 
 /*
-Cyclic Pursuit
+Moving centroid
+*/
+
+/*
+Changelog:
+-created
+
+Todo:
+start with moving centroid
+
 */
 
 #include <ros/ros.h>
@@ -13,12 +22,12 @@ Cyclic Pursuit
 #include <stdio.h>
 #include <stdlib.h>
 
-double rad1,x0, z;
+double cenPos;
 double yc;
 double rad2,x2,y2,radN;
 
 std::string name_, name2_;
-
+/*
 
 void selfCallback(const turtlebot_deployment::PoseWithName::ConstPtr& selfPtr)
 {
@@ -44,7 +53,9 @@ else
     }
 }
 }
+*/
 
+/*
 void allPoseCallback(const turtlebot_deployment::PoseWithName::ConstPtr& posePtr)
 {
     if (name_!=posePtr->name){
@@ -78,7 +89,7 @@ else
       
         /*if (rad2<radN){
           radN=rad2;
-        }*/
+        }*//*
         if (name2_=="temp"){
             name2_=posePtr->name;
         }
@@ -93,37 +104,49 @@ else
         }
     }
 }
-
+*/
 int main(int argc, char **argv)
 {
 ros::init(argc, argv, "cyclic_pursuit");
 ros::NodeHandle ph_, nh_;
-ros::Publisher vel_pub_;
-ros::Subscriber pos_sub_;
-ros::Subscriber self_sub_;
-geometry_msgs::Twist cmd_vel_;
+ros::Publisher cen_pub_;
+//ros::Subscriber pos_sub_;
+//ros::Subscriber self_sub_;
+//geometry_msgs::Twist cmd_vel_;
 ros::Rate loop_rate(.2);
-vel_pub_ = nh_.advertise<geometry_msgs::Twist>("velocity", 5, true);
-pos_sub_ = nh_.subscribe<turtlebot_deployment::PoseWithName>("/all_positions", 1000,allPoseCallback);
-self_sub_ = nh_.subscribe<turtlebot_deployment::PoseWithName>("afterKalman",1,selfCallback);
-cmd_vel_.linear.x=75;
-double k=1;
-radN=1;
-rad2=1;
-rad1=1;
-yc=0;
-y2=0;
-x0=0;
-x2=0;
-name2_="temp";
+cen_pub_ = nh_.advertise<turtlebot_deployment::PoseWithName>("centroidPos", 5, true);
+//pos_sub_ = nh_.subscribe<turtlebot_deployment::PoseWithName>("/all_positions", 1000,allPoseCallback);
+//self_sub_ = nh_.subscribe<turtlebot_deployment::PoseWithName>("afterKalman",1,selfCallback);
+//cmd_vel_.linear.x=75;
+//double k=1;
+//radN=1;
+//rad2=1;
+//rad1=1;
+//yc=0;
+//y2=0;
+//x0=0;
+//x2=0;
+//name2_="temp";
 while (1==1){
 
-    ros::spinOnce();
+    //ros::spinOnce();
     
-std::cout<<"RADIANS1: "<<rad1<<"\n";
-std::cout<<"RADIANS2: "<<rad2<<"\n";
-    cmd_vel_.linear.x=4*k*(radN);
-    vel_pub_.publish(cmd_vel_);
+//std::cout<<"RADIANS1: "<<rad1<<"\n";
+//std::cout<<"RADIANS2: "<<rad2<<"\n";
+ if (l==1){
+  cenPos=cenPos+10;
+ }else{
+     
+     cenPos=cenPos-10;
+ }
+ if (cenPos<200){
+     l==1;
+ }
+ if (cenPos>500){
+     l==0;
+ }
+ 
+    cen_pub_.publish(cenPose);
     usleep(100000);
     
 }
