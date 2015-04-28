691da3a3b52ec7a3b2889effd372767af6dda353
aaronma37 <aam021@eng.ucsd.edu>
2015-04-28 16:28:08 -0700

    Update movingCentroid.cpp

--- deployment/turtlebot_deployment/src/movingCentroid.cpp
+++ deployment/turtlebot_deployment/src/movingCentroid.cpp
@@ -118,7 +118,7 @@ else
 int main(int argc, char **argv)
 {
 ros::init(argc, argv, "movingCentroid");
-ros::NodeHandle ph_, gnh_;
+ros::NodeHandle ph_, gnh_, nh;
 ros::Publisher cen_pub_;
 std_msgs::Float64 floatMsg;
 turtlebot_deployment::PoseWithName cenPose;
