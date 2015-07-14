0dbe5497df28933378690decb4e580a7babed04f
aaronma37 <aam021@eng.ucsd.edu>
2015-07-14 15:48:48 -0700

    Update quadcopter_ekf.cpp

--- quadcopter_ekf.cpp
+++ quadcopter_ekf.cpp
@@ -25,41 +25,29 @@ ROS opensource
 */
 /*
 Notes for meeting-
--NEED TO FIX THE LAUNCH PROBLEM (SENDALL SEND SEND_)
+-Genesis
 */
 /*
 Changelog
-To Do
--State noise covariance needs to be determined
--state dynamics need to be determined
--Independent Timer
+
 -----------------------------------------------------------------------------------------
 BEGIN
 */
 using namespace std;
 using namespace Eigen;
 using Eigen::MatrixXd;
-//***Remove later
-/*-----------------------------------------------------------------------------------------
-** Initialization Block
-**
-** Initialize Q,R,W, I(identity) as a permanent matrices determined by
-** covariance in our modeladam lambert what do you want from me
-**
-** Initialize P,H,X,XT,A,K,Z as matrices that vary
-** during iterations
-*/
-Matrix3f Q= Matrix3f::Zero();
-Matrix3f R= Matrix3f::Zero();
-Matrix3f W= Matrix3f::Identity();
-Matrix3f I= Matrix3f::Identity();
-Matrix3f P= Matrix3f::Zero();
-Matrix3f H= Matrix3f::Identity();
-MatrixXf X(3,1);
+
+Matrix4f Q= Matrix4f::Zero();
+Matrix4f R= Matrix4f::Zero();
+Matrix4f W= Matrix4f::Identity();
+Matrix4f I= Matrix4f::Identity();
+Matrix4f P= Matrix4f::Zero();
+Matrix4f H= Matrix4f::Identity();
+MatrixXf X(4,1);
 VectorXf XT(3);
-Matrix3f A;
-Matrix3f K;
-VectorXf Z(3);
+Matrix4f A;
+Matrix4f K;
+VectorXf Z(4);
 vector<Vector3f> Xv;
 vector<Vector3f> XTv;
 vector<Matrix3f> Av;
@@ -69,296 +57,146 @@ vector<Matrix3f> Hv;
 vector<string> robots; //Initialize a vector of robot names
 std::string name_;
 turtlebot_deployment::PoseWithNamePtr newPose_;
+geometry_msgs::PoseStamped measurementPose;
+geometry_msgs::Twist twist;
+
+
 bool got_pose_, stationary;
 double theta,
 x,
 y;
 double T =20;
 int counter11=0;
-class agent{
-public:
-float x, y, theta, velo, omega, t;
-string name;
-};
-vector<agent> agentVector;
-class getName
-{
-public:
-getName();
-private:
-ros::NodeHandle ph1_, ph;
-std::string name1_;
-} ;
-getName::getName():
-ph1_("~"),
-name1_("no_name")
-{
-ph1_.param("robot_name", name1_, name1_);
-name_=name1_;
-}
-void poseCallback(const turtlebot_deployment::PoseWithName::ConstPtr& posePtr)
-{
-std::cout<<"name_ "<<name_<<"\n";
-std::cout<<posePtr->name<<"\n";
-int size = robots.size();
-int iTemp;
-agent a; //Temp agent used if a new agent is introduced
-if (posePtr->name==name_){
-iTemp=0;
-got_pose_=true;
-if (iTemp==size)
-{
-//Add elements to necessary vectors
-robots.push_back(posePtr->name); //Adds robot name element to vector<string>robots
-agentVector.push_back(a); //Adds class "agent" element to vector<agent>agentVector
-//Initialize new detected robot matrices and state
-P=Matrix3f::Zero(); //Initialize Matrix P(confidence) to be "loose"
-P(0,0)=900;
-P(1,1)=900;
-P(2,2)=900;
-X<<1,2,3; //Define initial position
-XT=X;
-//Add new agent's matrix elements to corresponding vectors
-Xv.push_back(X);
-Pv.push_back(P);
-XTv.push_back(XT);
-//Set new agent's position
-agentVector[iTemp].name=posePtr->name;
-agentVector[iTemp].x=posePtr->pose.position.x;
-agentVector[iTemp].y=posePtr->pose.position.y;
-agentVector[iTemp].theta = tf::getYaw(posePtr->pose.orientation)+3.14;
+double yaw;
 
-}
-else{
-/*
-** This block runs if posePtr->name was found in found, ie if robot's id was previously detected.
-*/
+void poseCallback(const geometry_msgs::PoseStamped::ConstPtr& posePtr)
+{
+got_pose_=true;
 std::cout<<"pass";
 //Set found agent's position
-agentVector[iTemp].x=posePtr->pose.position.x;
-agentVector[iTemp].y=posePtr->pose.position.y;
-agentVector[iTemp].theta = tf::getYaw(posePtr->pose.orientation)+3.14;
+//NOT SURE ABOUT PITCH AND ROLL
+measurementPose.pose.position=posePtr->pose.position;
+measurementPose.pose.orientation = posePtr->pose.orientation;
+
+yaw=tf::getYaw(posePtr->pose.orientation)+3.14;
 }
-}
-else{
-}
-}
+
+
+
 void iptCallback(const geometry_msgs::Twist::ConstPtr& ipt)
 {
-//Determine if subscribed .name is already in group. If not, this initiallizes a new one
-int size = robots.size();
-int iTemp;
-iTemp=0;
-agent a;
-/* -----------------------------------------------------------------------------------------
-** Same iTemp search algorithm as in poseCallback
-** ***This algorithm may become a separate function
-*/
-if (size==0){
-robots.push_back(name_);
-agentVector.push_back(a);
-agentVector[iTemp].name=name_;
-P=Matrix3f::Zero();
-P(0,0)=900;
-P(1,1)=900;
-P(2,2)=900;
-H=Matrix3f::Identity();
-Pv.push_back(P);
-Hv.push_back(H);
-X<<1,2,3;
-Xv.push_back(X);
-XT=X;
-XTv.push_back(XT);std::cout<<"Measured: \n"<<Z<<"\n\n";
-agentVector[iTemp].velo=ipt->linear.x;
-agentVector[iTemp].omega=ipt->angular.z;
+twist.linear=ipt->linear;
+twist.angular=ipt->angular;
 }
-else{
-agentVector[iTemp].velo=ipt->linear.x;
-agentVector[iTemp].omega=ipt->angular.z;
-}
-}
+
+
 int main(int argc, char **argv)
 {
-ros::init(argc, argv, "ekf_temp"); //Ros Initialize
+ros::init(argc, argv, "Quadcopter Extended Kalman Filter: version 1, ORB SLAM only"); //Ros Initialize
 ros::start();
 ros::Rate loop_rate(T); //Set Ros frequency to 50/s (fast)
-getName getname;
-ofstream myfile;
-const char *path="/home/turtlebot/ekfData.txt";
-double send_;
-myfile.open(path);
-myfile <<"Data";
-myfile.close();
+
 ros::NodeHandle nh_, ph_, gnh_, ph("~");
 ros::Subscriber pos_sub_ ;
 ros::Subscriber ipt_sub_ ;
 ros::Publisher gl_pub_ ;
 ros::Publisher sf_pub_;
 ros::Publisher nm_pub_;
-ros::Publisher cal0_pub_;
-ros::Publisher calD_pub_;
-ros::Publisher kalmanError;
-ph.getParam("sendAll", send_);
-void poseCallback(const turtlebot_deployment::PoseWithName::ConstPtr& pose);
+
+void poseCallback(const geometry_msgs::PoseStamped::ConstPtr& pose);
 void iptCallback(const geometry_msgs::Twist::ConstPtr&);
 // ROS stuff
 // Other member variables
 got_pose_=false;
 stationary=false;
+
 Q(0,0)=0;
 Q(1,1)=0;
 Q(2,2)=0;
 R(0,0)=.01;
 R(1,1)=.01;
 R(2,2)=.01;
-double OmegaC=1.5;
-double OmegaD=.7;
+P(0,0)=1000;
+P(1,1)=1000;
+P(2,2)=1000;
+P(3,3)=1000;
+
+
+
 double counter12=0;
-double x0=0;
-double y0=0;
-double ec=0;
-double ed=0;
-double id=.7;
-double ic=1.5;
-double ed0=0;
-double ec0=0;
+
 std_msgs::Float64 floatMsg, floatMsg2, ke;
-int iTemp;
-iTemp=0;
-int size = robots.size();
-pos_sub_= nh_.subscribe<turtlebot_deployment::PoseWithName>("toKalmanfilter", 1,poseCallback);
-ipt_sub_=nh_.subscribe<geometry_msgs::Twist>("mobile_base/commands/velocity",1,iptCallback);
-gl_pub_ = gnh_.advertise<turtlebot_deployment::PoseWithName>("/all_positions", 1000, true);
-sf_pub_= gnh_.advertise<turtlebot_deployment::PoseWithName>("afterKalman",1,true);
-nm_pub_= gnh_.advertise<turtlebot_deployment::PoseWithName>("nametest", 5);
-cal0_pub_= gnh_.advertise<std_msgs::Float64>("cal0", 1,true);
-calD_pub_= gnh_.advertise<std_msgs::Float64>("calD", 1,true);
-calD_pub_= gnh_.advertise<std_msgs::Float64>("calD", 1,true);
-kalmanError=gnh_.advertise<std_msgs::Float64>("ke",1,true);
+
+pos_sub_= nh_.subscribe<geometry_msgs::PoseStamped>("/tf", 1,poseCallback);
+ipt_sub_=nh_.subscribe<geometry_msgs::Twist>("/cmd_vel",1,iptCallback);
+gl_pub_ = gnh_.advertise<geometry_msgs::PoseStamped>("/poseEstimation", 1000, true);
+
 while (ros::ok()) {
 got_pose_=false;
 ros::spinOnce();
-if (got_pose_==true){
-cout<<"got_pose_: "<<got_pose_<<"\n";
-R(0,0)=1;
-R(1,1)=1;
-R(2,2)=1;
-}else{
-R(0,0)=10000;
-R(1,1)=10000;
-R(2,2)=10000;
-}
-if (stationary==true){
-Q(0,0)=0;
-Q(1,1)=0;
-Q(2,2)=0;
-}else{
-Q(0,0)=5;
-Q(1,1)=5;
-Q(2,2)=5;
-}
-if (0<robots.size()){
-///
+
+    //Conditionals
+    if (got_pose_==true){
+    cout<<"got_pose_: "<<got_pose_<<"\n";
+    R(0,0)=1;
+    R(1,1)=1;
+    R(2,2)=1;
+    }else{
+    R(0,0)=10000;
+    R(1,1)=10000;
+    R(2,2)=10000;
+    }
+    if (stationary==true){
+    Q(0,0)=0;
+    Q(1,1)=0;
+    Q(2,2)=0;
+    }else{
+    Q(0,0)=5;
+    Q(1,1)=5;
+    Q(2,2)=5;
+    }
+
 VectorXf Z(3);
 Matrix3f temp;
 P=Pv[iTemp];
-//Stage 1
-Z << agentVector[iTemp].x,agentVector[iTemp].y,agentVector[iTemp].theta;
 
-//Calibration 
-if (counter11>10){
-//XT << XT(0)+agentVector[iTemp].velo*167/T*cos(XT(2))/OmegaD,XT(1)+agentVector[iTemp].velo*167/T*sin(XT(2))/OmegaD,XT(2)+agentVector[iTemp].omega*57/52/T/OmegaC;
-XT << XT(0)+agentVector[iTemp].velo*167/T*cos(XT(2)),XT(1)+agentVector[iTemp].velo*167/T*sin(XT(2)),XT(2)+agentVector[iTemp].omega*57/52/T;
-    
-}
-//X << X(0)+agentVector[iTemp].velo*167/T*cos(X(2))/OmegaD,X(1)+agentVector[iTemp].velo*167/T*sin(X(2))/OmegaD,X(2)+agentVector[iTemp].omega*57/52/T/OmegaC;
-X << X(0)+agentVector[iTemp].velo*167/T*cos(X(2)),X(1)+agentVector[iTemp].velo*167/T*sin(X(2)),X(2)+agentVector[iTemp].omega*57/52/T;
-cout<<"Velocity: "<<agentVector[iTemp].velo*167/T<<"\n";
+//Stage 1
+Z << measurementPose.pose.position.x,measurementPose.pose.position.y,measurementPose.pose.position.z,yaw;
+X << X(0)+twist.linear.x/T,X(1)+twist.linear.y/T,X(2)+twist.linear.z/T,X(3)+twist.angular.z/T;
+
 //Stage 2
 if (got_pose_==true){
-A << 1, 0, -agentVector[iTemp].velo*167/T*sin(agentVector[iTemp].theta),0, 1,agentVector[iTemp].velo*167/T*cos(agentVector[iTemp].theta),0, 0, 1;
+A << 1, 0,0, -twist.linear.x/T*sin(yaw)+twist.linear.y/T*cos(yaw),0, 1,0, twist.linear.x/T*cos(yaw)-twist.linear.y/T*sin(yaw),0, 0, 1,0;
 P=A*P*A.transpose()+W*Q*W.transpose();
+
 //Stage 3
 temp=(W*P*W.transpose()+W*R*W.transpose());
 K=P*W.transpose()*temp.inverse();
+
 //Stage 4
 X=X+K*(Z-X);
+
 //Stage 5
 P=(I-K*W)*P;
-
-//PID FEEDBACK
-    if (counter12+5<counter11){
-        ed=(sqrt((XT(1)-y0)*(XT(1)-y0)+(XT(0)-x0)*(XT(0)-x0))-sqrt((X(1)-y0)*(X(1)-y0)+(X(0)-x0)*(X(0)-x0)));
-        //NEED TO MAKE ARCLENGTH NOT SQRT
-        id=id+(counter11-counter12)*ed/1500;
-      OmegaD=1+.01*ed+.01*id+0*(ed-ed0);//PID
-      
-       ed0=ed;
-      if ((XT(2)-X(2))>3.14){
-        ec=((XT(2))-(X(2)+2*3.14));
-    //OmegaC=OmegaC+.2*((XT(2))-(X(2)+2*3.14));
-          ke.data = XT(2)-(X(2)+2*3.14);
-      }
-    else{
-        ec=((XT(2))-(X(2)));
-        //OmegaC=OmegaC+.2*((XT(2))-(X(2)));
-        ke.data=XT(2)-X(2);
-    }
-    ic=ic+(counter11-counter12)*ec/250;
-    OmegaC=.02*ec+1*ic+0*(ec-ec0);
-    ec0=ec;
-    
-    
-    
-      XT=X;
-      counter12=counter11;
-      if (id<-25){id=-25;}
-      if (id>25){id=25;}
-      if (ic<-25){ic=-25;}
-      if (ic>25){ic=25;}
-      floatMsg.data=OmegaC;
-      floatMsg2.data=OmegaD;
-      cal0_pub_.publish(floatMsg);
-      calD_pub_.publish(floatMsg2);
-      kalmanError.publish(ke);
-      x0=X(0);
-      y0=X(1);
-      
-    }
 }
 
 //Set Vectors
 Xv[iTemp]=X;
 Pv[iTemp]=P;
-turtlebot_deployment::PoseWithName goalPose;
-goalPose.pose.position.x = X(0);
-goalPose.pose.position.y = X(1);
-goalPose.name=agentVector[iTemp].name;
-goalPose.pose.orientation =tf::createQuaternionMsgFromYaw(X(2)+3.14);
-//added if Statement
-//if (goalPose.name!=""){
-if (send_==1){
-gl_pub_.publish(goalPose);
-}
-goalPose.name=name_;
-nm_pub_.publish(goalPose);
-sf_pub_.publish(goalPose);
-//}
-counter11=counter11+1;
-cout<<"Counter: "<<counter11<<"\n";
-cout<<"Number of Robots: "<<size<<"\n";
-cout<<"Robot #: "<<iTemp<<"\n";
-std::cout<<"Measured: \n"<<Z<<"\n\n";
-std::cout<<"OmegaC\n"<<OmegaC<<"\n---------\n";
-std::cout<<"OmegaD\n"<<OmegaD<<"\n---------\n";
-std::cout<<"Goal Pose\n"<<goalPose<<"\n---------\n\n\n\n";
+geometry_msgs::PoseStamped poseEstimation;
+poseEstimation.pose.position.x = X(0);
+poseEstimation.pose.position.y = X(1);
+poseEstimation.pose.position.z = X(2);
+goalPose.pose.orientation =tf::createQuaternionMsgFromYaw(X(3)+3.14);
+gl_pub_.publish(poseEstimation);
 
+
+std::cout<<"Measured: \n"<<Z<<"\n\n";
+std::cout<<"Twist: \n"<<twist<<"\n\n";
+std::cout<<"Best Estimation\n"<<poseEstimation<<"\n---------\n\n\n\n";
 std::cout<<"--------------------------------------------------------------------";
-//}
-///
 loop_rate.sleep();
-//sleep(1/50);
-}
+
 }
 }
 //END
