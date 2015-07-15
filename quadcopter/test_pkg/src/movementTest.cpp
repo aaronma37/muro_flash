eac87c4b4df3a9614845ec4cdb7fa13f3b597570
aaronma37 <aam021@eng.ucsd.edu>
2015-07-15 13:14:43 -0700

    Update movementTest.cpp

--- quadcopter/test_pkg/src/movementTest.cpp
+++ quadcopter/test_pkg/src/movementTest.cpp
@@ -1,1 +1,38 @@
 
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include <stdlib.h>
+#include <sstream>
+#include <tf/tf.h>
+#include "geometry_msgs/PoseWithCovarianceStamped.h"
+#include "geometry_msgs/Pose.h"	
+#include "geometry_msgs/PoseStamped.h"	
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "movement test");
+  ros::NodeHandle n;
+  ros::Publisher chatter_pub = n.advertise<std_msgs::Empty>("/ardrone/takeoff", 1000);
+  ros::Publisher chatter_pub2 = n.advertise<std_msgs::Empty>("/ardrone/land", 1000);
+
+ros::Rate loop_rate(200);
+int count =0;
+chatter_pub.publish();
+ while (ros::ok()){
+  
+  if (count > 1000){
+    chatter_pub2.publish();
+    break;
+  }
+  
+  count=count+1;
+  loop_rate.sleep();
+  }
+}
