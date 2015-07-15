721aff68bdc86e87249a8ac37f56766807416a9a
aaronma37 <aam021@eng.ucsd.edu>
2015-07-14 22:11:32 -0700

    Update quadcopter_ekf.cpp

--- quadcopter/test_pkg/src/quadcopter_ekf.cpp
+++ quadcopter/test_pkg/src/quadcopter_ekf.cpp
@@ -63,12 +63,13 @@ double yaw; // FIXME: What is this for?
 // Updates position coordinates
 void poseCallback(const tf2_msgs::TFMessage::ConstPtr& posePtr)
 {
-    if (posePtr->transforms->header.frame_id.compare("ORB_SLAM/World")==0){
+    const tf2_msgs::TFMessage& msg=*posePtr;
+    if (posePtr->msg.transforms.header.frame_id.compare("ORB_SLAM/World")==0){
     got_pose_ = true;
     std::cout<<"pass";
     // FIXME: Set found agent's position
     // FIXME: NOT SURE ABOUT PITCH AND ROLL
-    measurementPose.pose.position = posePtr->transforms.transform.translation;
+    measurementPose.pose.position = msg.transforms.transform.translation;
     measurementPose.pose.orientation = posePtr->transforms.transform.rotation;
 
     yaw = tf::getYaw(posePtr->st_transforms.transform.rotation)+3.14;
