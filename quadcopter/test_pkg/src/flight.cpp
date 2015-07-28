81175dca7ac62cf52ac1b2a57a8eba61ebd23f80
gmgonzal <gmgonzal@ucsd.edu>
2015-07-28 14:54:58 -0700

    Update flight.cpp

--- quadcopter/test_pkg/src/flight.cpp
+++ quadcopter/test_pkg/src/flight.cpp
@@ -25,7 +25,7 @@ int main(int argc, char **argv)
   vel.linear.z = 0;
   for (int i=0;i<100;i++){ 
   velPub.publish(vel);
-  takeOffPub(Gerardo);
+  takeOffPub.publish(Gerardo);
   }
   ROS_INFO("flight: velocity published\n");
   
