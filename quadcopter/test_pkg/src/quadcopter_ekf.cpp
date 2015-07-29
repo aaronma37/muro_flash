aeacf2bf9658e7f38833f1cc9e4e9d811df4a7ce
aaronma37 <aam021@eng.ucsd.edu>
2015-07-28 18:38:14 -0700

    Update quadcopter_ekf.cpp

--- quadcopter/test_pkg/src/quadcopter_ekf.cpp
+++ quadcopter/test_pkg/src/quadcopter_ekf.cpp
@@ -103,7 +103,7 @@ void poseCallback(const tf2_msgs::TFMessage::ConstPtr& posePtr)
 void imuCallback(const ardrone_autonomy::Navdata::ConstPtr& imuPtr)
 {
     got_vel_ = true;
-std::cout<<imuPtr;
+std::cout<<imuPtr;
     measurementTwist.linear.x= imuPtr->vx/1000;    
     measurementTwist.linear.y= imuPtr->vy/1000;
     measurementTwist.linear.z= imuPtr->vz/1000;
@@ -127,7 +127,7 @@ int main(int argc, char **argv)
     ros::NodeHandle nh_, ph_, gnh_, ph("~");
     ros::Subscriber pos_sub_ ;
     ros::Subscriber ipt_sub_ ;
-    ros::Subscriber imu_sub_ ;
+    ros::Subscriber imu_sub_ ;
     ros::Publisher gl_pub_ , vel_pub_;
 
     void poseCallback(const tf2_msgs::TFMessage::ConstPtr& pose);
