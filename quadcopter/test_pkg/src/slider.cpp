b7792d61a50dbaec3ee3501617f60f653fa8148a
gmgonzal <gmgonzal@ucsd.edu>
2015-07-30 14:09:32 -0700

    Update slider.cpp

--- quadcopter/test_pkg/src/slider.cpp
+++ quadcopter/test_pkg/src/slider.cpp
@@ -277,19 +277,19 @@ void PID(void)
     {
       sX = -sliderSlope*poseError.pose.position.x + velEstimation.linear.x;
     }
-    else sX = -( poseError.pose.position.x/( sqrt(abs(poseError.pose.position.x)) ) ) + velEstimation.linear.x;
+    else sX = -0.5*( ( poseError.pose.position.x/( sqrt(abs(poseError.pose.position.x)) ) ) + velEstimation.linear.x );
     
     if(poseError.pose.position.y < sliderRange && poseError.pose.position.y > -sliderRange)
     {
       sY = -sliderSlope*poseError.pose.position.y + velEstimation.linear.y;
     }
-    else sY = -( poseError.pose.position.y/( sqrt(abs(poseError.pose.position.y)) ) ) + velEstimation.linear.y;
+    else sY = -0.5*( ( poseError.pose.position.y/( sqrt(abs(poseError.pose.position.y)) ) ) + velEstimation.linear.y );
     
     if(poseError.pose.position.z < sliderRange && poseError.pose.position.z > -sliderRange)
     {
       sZ = -sliderSlope*poseError.pose.position.z + velEstimation.linear.z;
     }
-    else sZ = -( poseError.pose.position.z/( sqrt(abs(poseError.pose.position.z)) ) ) + velEstimation.linear.z;
+    else sZ = -0.5*( ( poseError.pose.position.z/( sqrt(abs(poseError.pose.position.z)) ) ) + velEstimation.linear.z );
     //*/
       
     velocity.linear.x = sX*(-sliderGain);
