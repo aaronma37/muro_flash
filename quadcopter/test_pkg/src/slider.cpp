130fc6481e5768161a91366166244d5ed429d2c9
gmgonzal <gmgonzal@ucsd.edu>
2015-08-04 15:56:43 -0700

    Update slider.cpp

--- quadcopter/test_pkg/src/slider.cpp
+++ quadcopter/test_pkg/src/slider.cpp
@@ -321,6 +321,11 @@ void PID(void)
       velocity.linear.z = -1;
     }
     
+    // Coordinate transformation from global to local coordinates
+    double tempX = velocity.linear.x;
+    velocity.linear.x = velocity.linear.x*cos(poseEstYaw) + velocity.linear.y*sin(poseEstYaw);
+    velocity.linear.y = (-tempX*sin(poseEstYaw)) + velocity.linear.y*cos(poseEstYaw);
+    
     velocity.angular.z = (kpYaw*poseErrYaw) + (kiYaw*pastYawErr) + (kdYaw*T*(maResults[3]));
     
     // For modeling purposes
