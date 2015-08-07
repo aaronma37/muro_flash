446ead83373bade90b92e2e6dce6054e896abe94
gmgonzal <gmgonzal@ucsd.edu>
2015-08-06 17:05:46 -0700

    Update path_follower.cpp

--- quadcopter/test_pkg/src/path_follower.cpp
+++ quadcopter/test_pkg/src/path_follower.cpp
@@ -20,10 +20,10 @@ goal pose in order to execute a path following algorithm.
 #include <sstream>
 
 // Quadcopter state data
-geometry_msgs::PoseStamped* poseEst; 
+geometry_msgs::PoseStamped poseEst; 
 
 // Path data
-geometry_msgs::Pose[] pathPose;
+geometry_msgs::Pose pathPose[];
 
 // Constants
 const double PI = 3.141592653589793238463;
