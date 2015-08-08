de51a6f5e42dd0855228a617b38d006e24794eae
gmgonzal <gmgonzal@ucsd.edu>
2015-08-07 17:28:11 -0700

    Update path_follower_test.cpp

--- quadcopter/test_pkg/src/path_follower_test.cpp
+++ quadcopter/test_pkg/src/path_follower_test.cpp
@@ -1,1 +1,42 @@
+#include <iostream>
+#include <stdio.h>
+#include <ros/ros.h>
+#include <geometry_msgs/PoseArray.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/Pose.h>
+#include <math.h>
+#include <vector>
+#include <stdlib.h>
+#include "std_msgs/String.h"
+#include <std_msgs/Float64.h>
+#include <std_msgs/Bool.h>
+#include <sstream>
 
+// Test msg
+geometry_msgs::PoseArray testPose;
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "path_follower"); //Ros Initialize
+    ros::start();
+    ros::Rate loop_rate(T); //Set Ros frequency to 50/s (fast)
+
+    ros::NodeHandle n;
+    ros::Publisher testPub;
+
+    goalPub = n.advertise<geometry_msgs::PoseArray>("/path", 1000, true);
+
+    // Initialize test msg
+    testPose.header.frame_id = "OPEN";
+    for(int i = 0; i < 10; i++)
+    {
+      (testPose.poses)[i].position.x = i;
+      (testPose.poses)[i].position.y = i;
+      (testPose.poses)[i].position.z = i;
+    }
+    
+    goalPub.publish(testPose);
+    
+    loop_rate.sleep();
+}
+//END
