dc823840679d0c178d856282007e3dbb61bab47a
gmgonzal <gmgonzal@ucsd.edu>
2015-08-10 13:08:03 -0700

    Update pf_test.cpp

--- quadcopter/test_pkg/src/pf_test.cpp
+++ quadcopter/test_pkg/src/pf_test.cpp
@@ -28,7 +28,7 @@ int main(int argc, char **argv)
     ros::NodeHandle n;
     ros::Publisher testPub;
     
-    testPose.poses = new geometry_msgs::Pose[10];
+    testPose.poses = poseArray;
 
     testPub = n.advertise<geometry_msgs::PoseArray>("/path", 1000, true);
 
