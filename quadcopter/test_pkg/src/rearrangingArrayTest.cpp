9f29191fc73e72480992f3be5dc01894f5e2bfc5
Jose Ramirez <joseluisjunior1993@hotmail.com>
2015-08-18 15:08:53 -0700

    Update rearrangingArrayTest.cpp

--- quadcopter/test_pkg/src/rearrangingArrayTest.cpp
+++ quadcopter/test_pkg/src/rearrangingArrayTest.cpp
@@ -21,12 +21,13 @@ double placeHolder[15] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 int chosenIndex = 5;
 int main()
 {
+    placeHolder [0] = originalArray[0];
     originalArray[0] = originalArray[chosenIndex];
     int j = 1;
     int z = 0;
     while (originalArray[chosenIndex+j] != 0 )
     {
-        placeHolder[j-1] = originalArray[j];
+        placeHolder[j] = originalArray[j];
         originalArray[j] = originalArray[chosenIndex+j];
         j++;
     }
