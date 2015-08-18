757e4791d6149cc0a7a1f3a5eabd19e09d27d413
aaronma37 <aam021@eng.ucsd.edu>
2015-08-18 16:33:39 -0700

    Update hover.cpp

--- quadcopter/test_pkg/src/hover.cpp
+++ quadcopter/test_pkg/src/hover.cpp
@@ -291,8 +291,8 @@ void PID(void)
     
     
     double tempX = velocity.linear.x;
-    velocity.linear.x =  velocity.linear.x*cos(poseEstYaw+PI) + velocity.linear.y*sin(poseEstYaw+PI);
-    velocity.linear.y = (tempX*sin(poseEstYaw+PI)) +  velocity.linear.y*cos(poseEstYaw+PI);
+    velocity.linear.x =  velocity.linear.x*cos(poseEstYaw+PI) - velocity.linear.y*sin(poseEstYaw+PI);
+    velocity.linear.y = (tempX*sin(poseEstYaw+PI)) -  velocity.linear.y*cos(poseEstYaw+PI);
     
     // For modeling purposes
     velPoseEstX.x = poseEstimation.pose.position.x;
