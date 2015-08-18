f3387c02e755b06050b8b6ddbe5d9ddd94759ff4
Jose Ramirez <joseluisjunior1993@hotmail.com>
2015-08-18 14:31:12 -0700

    Create rearrangingArrayTest.cpp

---
+++ quadcopter/test_pkg/src/rearrangingArrayTest.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdio.h>
+#include <ros/ros.h>
+#include <geometry_msgs/PoseArray.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/Pose.h>
+#include <math.h>
+#include <vector>
+#include <stdlib.h>
+#include "std_msgs/String.h"
+#include <std_msgs/Float64.h>
+#include <std_msgs/Bool.h>
+#include <sstream>
+
+
+// Variables
+double originalArray[15] = {1,2,3,4,5,6,7,8,9,10,11,0,0,0,0};
+
+int main()
+{
+    return;
+    
+}
