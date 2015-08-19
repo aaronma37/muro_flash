f6124f4b78c17b315cb1d5444538851b0a0996fe
aaronma37 <aam021@eng.ucsd.edu>
2015-08-18 18:05:04 -0700

    Update slider.cpp

--- quadcopter/test_pkg/src/slider.cpp
+++ quadcopter/test_pkg/src/slider.cpp
@@ -338,12 +338,12 @@ void PID(void)
     
     
     if (velocity.linear.x!=0 && velocity.linear.y!=0){
-      activeAngle=(acos(velocity.linear.x/sqrt(velocity.linear.x*velocity.linear.x+velocity.linear.y*velocity.linear.y)))-poseEstYaw;
+      activeAngle=(acos(velocity.linear.x/sqrt(velocity.linear.x*velocity.linear.x+velocity.linear.y*velocity.linear.y)))-poseEstYaw+PI;
     std::cout<<"Vx: \n"<<velocity.linear.x<<"\n\n";
     std::cout<<"Vy: \n"<<velocity.linear.y<<"\n\n";
     std::cout<<"Active Angle: \n"<<(acos(velocity.linear.x/sqrt(velocity.linear.x*velocity.linear.x+velocity.linear.y*velocity.linear.y)))<<"\n\n";
     double tempX = velocity.linear.x;
-    velocity.linear.x =  -velocity.linear.x*cos(activeAngle) + velocity.linear.y*sin(activeAngle);
+    velocity.linear.x =  velocity.linear.x*cos(activeAngle) + velocity.linear.y*sin(activeAngle);
     velocity.linear.y = (-tempX*sin(activeAngle)) +  velocity.linear.y*cos(activeAngle);
     }
     
