6e72f25d99750bbcbef1c25e65b0b0c0eb6d9950
gmgonzal <gmgonzal@ucsd.edu>
2015-08-21 15:59:54 -0700

    Update pf_test.cpp

--- quadcopter/test_pkg/src/pf_test.cpp
+++ quadcopter/test_pkg/src/pf_test.cpp
@@ -16,48 +16,71 @@
 // Test msg
 geometry_msgs::PoseArray testPose;
 geometry_msgs::Pose poseArray[10];
+geometry_msgs::PoseStamped poseEst;
 
 // Constants
 const double T = 1;
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "path_follower"); //Ros Initialize
+    ros::init(argc, argv, "path_follower_test"); //Ros Initialize
     ros::start();
     ros::Rate loop_rate(T); //Set Ros frequency to 50/s (fast)
 
     ros::NodeHandle n;
     ros::Publisher testPub;
+    ros::Publisher poseEstPub;
     
     //testPose.poses = poseArray;
 
     testPub = n.advertise<geometry_msgs::PoseArray>("/path", 1000, true);
+    poseEstPub = n.advertise<geometry_msgs::PoseStamped>("/poseEstimation", 1000, true);
 
     // Initialize test msg
     testPose.header.frame_id = "CLOSED";
-    for(int i = 0; i < 10; i++)
+    for(int i = 0; i < 5; i++)
     {
         testPose.poses.push_back(poseArray[i]);
     }
-    for(int i = 0; i < 10; i++)
+    for(int i = 0; i < 5; i++)
     {
-      (testPose.poses[i]).position.x = i;
-      (testPose.poses[i]).position.y = i;
-      (testPose.poses[i]).position.z = i;
+      if(i == 0)
+      {
+        (testPose.poses[i]).position.x = 0.01;
+        (testPose.poses[i]).position.y = 0.01;
+      }
+      else if(i == 1)
+      {
+        (testPose.poses[i]).position.x = 3;
+        (testPose.poses[i]).position.y = 0;  
+      }
+      else if(i == 2)
+      {
+        (testPose.poses[i]).position.x = 3;
+        (testPose.poses[i]).position.y = 3;  
+      }
+      else if(i == 3)
+      {
+        (testPose.poses[i]).position.x = 0;
+        (testPose.poses[i]).position.y = 3;  
+      }
+      else if(i == 4)
+      {
+        (testPose.poses[i]).position.x = 0;
+        (testPose.poses[i]).position.y = 0;  
+      }
+      (testPose.poses[i]).position.z = 0;
     }
+    poseEst.pose.position.x = 2.0;
+    poseEst.pose.position.y = 2.0;
+    poseEst.pose.position.z = 0.0;
+    poseEst.pose.orientation = tf::createQuaternionMsgFromYaw(0);
     
+    poseEstPub.publish(poseEst);
     testPub.publish(testPose);
    
-    ros::Duration d = ros::Duration(10, 0);
-    d.sleep();
-    
-    testPose.header.frame_id = "OPEN";
-    for(int i = 0; i < 10; i++)
-    {
-        testPose.poses.push_back(poseArray[i]);
-    }
-    
-    testPub.publish(testPose);
+    //ros::Duration d = ros::Duration(10, 0);
+    //d.sleep();
     
     ros::shutdown();
     return 0;
