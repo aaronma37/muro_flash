c79b068338bc13c09c1f023d86c0aa80649399d5
Mike Liu <shl202@mencia.(none)>
2015-09-03 19:35:42 -0700

    added meter to pixel conversion

--- deployment/turtlebot_move_to_goal/src/dubins_curve.cpp
+++ deployment/turtlebot_move_to_goal/src/dubins_curve.cpp
@@ -12,10 +12,12 @@
 #include "dubins.h"
 #include "nav_msgs/Path.h"
 
+#define TOPIXELS 231  //approximate pixels per meter at 2.7 meter high
+
 //TODO find the minimum turning radius
-const double mtr = 10;
+const double mtr = TOPIXELS(0.1);
 //TODO find the best step size of the path sample
-const double stepSize = 0.1;
+const double stepSize = TOPIXELS(0.004);
 //counter to determine when to publish the complete path
 int counter = 0;
 
